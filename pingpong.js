bd3dcb11dd862e7778bb2a2386160126e020fa38
DavidPrimata <168776694+DavidPrimata@users.noreply.github.com>
2024-06-27 17:41:57 -0300

    cria bolinha 27/06/24

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,4 @@
+//aqui 
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -6,4 +7,11 @@ function setup(){
 function draw(){
     //aqui vou por a cor da "mesa"
     background(224,255,255);
+    //chamando a função
+    criaBolinha(128,128,0)
+}
+
+//função bolinha
+function criaBolinha(xBolinha, yBolinha, tamBolinha){
+    circle (xBolinha, yBolinha, tamBolinha);
 }
